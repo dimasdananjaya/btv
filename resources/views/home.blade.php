01c4d59f8a51c41495e3721cff2c0c2fd73fbebb
dimasdananjaya <gededimas@gmail.com>
2020-11-14 23:28:36 +0800

    adding support local banner

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,9 @@
                 <h2 class="text-center">More Places To Explore</h2>
             </div><!--col-lg-4-->
             <div class="col-lg-8">
-                <p>We share our experience traveling around villages in Bali worthy visiting</p>
+                <p class="text-justify">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
             </div><!--col-lg-8-->
         </div><!--row-->
 
@@ -52,5 +54,13 @@
 
         </div><!--carousel-->
     </div><!--container-->
+    <div class="support-locals-banner mt-5">
+        <div class="black-layer">
+            <div class="container">
+                <h1 class="display-4">RUN BY THE LOCALS</h1>
+                <p class="lead reduce-top-margin text-center">Your visit will have a generous impact for the local people</p>
+            </div><!--container-->
+        </div><!--black layer-->
+    </div><!--end support locals-->
 </section>
 @endsection
